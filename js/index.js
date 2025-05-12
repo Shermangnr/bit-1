ca9b0b264c7f241657975030650749b357e78fb1
German Bermudez <shermangnr9@gmail.com>
2025-05-12 10:44:04 -0500

    Modificacion de archivo js para la pagina principal

--- js/index.js
+++ js/index.js
@@ -1,21 +1,5 @@
 'use strict';
 
-// const $formularioContacto = document.getElementById("formularioContacto");
-
-// $formularioContacto.addEventListener("submit", (event) => {
-//     event.preventDefault();
-    
-//     const nombre = $formularioContacto.nombre.value;
-//     const mensaje = $formularioContacto.mensaje.value;
-
-//     console.log(nombre);
-//     console.log(mensaje);
-    
-//     const result = `${nombre}, gracias por tu mensaje: ${mensaje}, Pronto te respondere!. `;
-//     alert(result);
-//     $formularioContacto.reset();
-// });
-
 //Efecto de vidrio al hacer scroll
 window.addEventListener("scroll", function () {
     const nav = document.querySelector(".menu-navegacion");
@@ -26,17 +10,4 @@ window.addEventListener("scroll", function () {
     }
 });
 
-// Mostrar 3 tarjetas al mismo tiempo
-// const carousel = document.querySelector('#productCarousel .carousel-inner');
-// const items = document.querySelectorAll('#productCarousel .carousel-item');
-// const cardWidth = items[0].offsetWidth;
-// const visibleCards = 3;
-
-// // Clona tarjetas para mantener al menos 3 visibles
-// items.forEach((el, index) => {
-// for (let i = 1; i < visibleCards; i++) {
-//     let next = items[(index + i) % items.length].cloneNode(true);
-//     el.querySelector('.row').appendChild(next.querySelector('.col-md-4'));
-// }
-// });
 
