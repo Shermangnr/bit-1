e9127cba9fc3379ac989acd9df3cd528e1d1499a
German Bermudez <shermangnr9@gmail.com>
2025-05-12 16:33:22 -0500

    Manejo de alertas para la pagina de contacto

--- js/contacto.js
+++ js/contacto.js
@@ -10,7 +10,42 @@ $formularioContacto.addEventListener("submit", (event) => {
     console.log(nombre);
     console.log(mensaje);
 
-    const result = `${nombre}, gracias por tu mensaje: ${mensaje}, Pronto te respondere!. `;
+    const result = `Hola ${nombre}, gracias por tu mensaje: ${mensaje}, Pronto te respondere!. `;
     alert(result);
     $formularioContacto.reset();
+});
+
+//Efecto de vidrio al hacer scroll
+window.addEventListener("scroll", function () {
+    const nav = document.querySelector(".menu-navegacion");
+    if (window.scrollY > 2) {
+    nav.classList.add("glass");
+    } else {
+    nav.classList.remove("glass");
+    }
+});
+
+//Alerta de bootstrap 5
+document.addEventListener("DOMContentLoaded", function () {
+    const alerta = document.getElementById("alerta-construccion");
+    const enlaces = document.querySelectorAll('.nav-link[href="#"]');
+
+    enlaces.forEach(enlace => {
+        enlace.addEventListener("click", function (e) {
+            e.preventDefault();
+            
+            alerta.classList.remove("d-none"); // Mostrar
+            // Forzar reflujo para reiniciar animación si se muestra varias veces
+            void alerta.offsetWidth;
+            alerta.classList.add("show");
+
+            // Ocultar después de 5 segundos
+            setTimeout(() => {
+                alerta.classList.remove("show");
+                setTimeout(() => {
+                    alerta.classList.add("d-none");
+                }, 500); // Esperar la transición antes de ocultar
+            }, 3000);
+        });
+    });
 });
